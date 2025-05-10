e859a547f5f2c395fa5e6390ec36be8410d0da7d
Anubhav Dhawan <anubhavdhawan@google.com>
2025-05-10 10:27:12 +0530

    chore: Restore add_auth_token(s) as deprecated for backward compatibility
    This PR addresses a breaking change introduced by the removal of `add_auth_token(s)` methods in #182. To ensure backward compatibility and facilitate a smoother upgrade path, these methods are now reintroduced but explicitly marked as **deprecated**. Users are encouraged to migrate to the new `add_auth_token_getter(s)` methods instead.
    
    > [!NOTE]
    > The `strict` flag in the deprecated methods are not used anymore since the functionality of the `strict` flag has been changed (see #205 for more details).

--- packages/toolbox-langchain/src/toolbox_langchain/tools.py
+++ packages/toolbox-langchain/src/toolbox_langchain/tools.py
@@ -15,6 +15,7 @@
 import asyncio
 from typing import Any, Callable, Union
 
+from deprecated import deprecated
 from langchain_core.tools import BaseTool
 from toolbox_core.sync_tool import ToolboxSyncTool as ToolboxCoreSyncTool
 
@@ -101,6 +102,18 @@ class ToolboxTool(BaseTool):
             ValueError: If the provided auth parameter is already registered.
         """
         return self.add_auth_token_getters({auth_source: get_id_token})
+
+    @deprecated("Please use `add_auth_token_getters` instead.")
+    def add_auth_tokens(
+        self, auth_tokens: dict[str, Callable[[], str]], strict: bool = True
+    ) -> "ToolboxTool":
+        return self.add_auth_token_getters(auth_tokens)
+
+    @deprecated("Please use `add_auth_token_getter` instead.")
+    def add_auth_token(
+        self, auth_source: str, get_id_token: Callable[[], str], strict: bool = True
+    ) -> "ToolboxTool":
+        return self.add_auth_token_getter(auth_source, get_id_token)
 
     def bind_params(
         self,
