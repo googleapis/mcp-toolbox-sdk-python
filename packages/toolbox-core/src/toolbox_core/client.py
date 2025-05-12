4738f4dfd580108b06fe2709c2e2700d3479834d
Anubhav Dhawan <anubhavdhawan@google.com>
2025-05-12 10:26:42 +0530

    chore: Unpack tuple instead of index it
    This makes it more readable.

--- packages/toolbox-core/src/toolbox_core/client.py
+++ packages/toolbox-core/src/toolbox_core/client.py
@@ -79,9 +79,9 @@ class ToolboxClient:
             else:  # regular parameter
                 params.append(p)
 
-        authn_params = identify_required_authn_params(
+        authn_params, _ = identify_required_authn_params(
             authn_params, auth_token_getters.keys()
-        )[0]
+        )
 
         tool = ToolboxTool(
             session=self.__session,
