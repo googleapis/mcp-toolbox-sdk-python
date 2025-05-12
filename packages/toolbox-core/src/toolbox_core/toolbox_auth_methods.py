874f2f173c72cfeb672331f9e73ae2bb0b90e252
Twisha Bansal <twishabansal@google.com>
2025-05-12 14:02:18 +0530

    cleanup

--- packages/toolbox-core/src/toolbox_core/toolbox_auth_methods.py
+++ packages/toolbox-core/src/toolbox_core/toolbox_auth_methods.py
@@ -1,20 +1,24 @@
 from functools import partial
+
+import google.auth
+from google.auth._credentials_async import Credentials
 from google.auth._default_async import default_async
 from google.auth.transport import _aiohttp_requests
-import google.auth 
-from google.auth.transport.requests import AuthorizedSession, Request 
-from google.auth._credentials_async import Credentials
+from google.auth.transport.requests import AuthorizedSession, Request
 
 
 async def aget_google_id_token():
     creds, _ = default_async()
     await creds.refresh(_aiohttp_requests.Request())
     creds.before_request = partial(Credentials.before_request, creds)
-    return creds.id_token
+    token = creds.id_token
+    return f"Bearer {token}"
+
 
 def get_google_id_token():
     credentials, _ = google.auth.default()
-    session = AuthorizedSession(credentials) 
-    request = Request(session) 
-    credentials.refresh(request) 
-    return credentials.id_token
+    session = AuthorizedSession(credentials)
+    request = Request(session)
+    credentials.refresh(request)
+    token = credentials.id_token
+    return f"Bearer {token}"
