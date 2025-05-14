ba29e2d635d4360d36a71e43b7f9262dc4a92507
Anubhav Dhawan <anubhavdhawan@google.com>
2025-05-14 18:52:57 +0530

    chore: Rename internal member variable names to be more concise

--- packages/toolbox-langchain/tests/test_e2e.py
+++ packages/toolbox-langchain/tests/test_e2e.py
@@ -53,7 +53,7 @@ class TestE2EClientAsync:
     @pytest_asyncio.fixture(scope="function")
     async def get_n_rows_tool(self, toolbox):
         tool = await toolbox.aload_tool("get-n-rows")
-        assert tool._ToolboxTool__core_sync_tool.__name__ == "get-n-rows"
+        assert tool._ToolboxTool__core_tool.__name__ == "get-n-rows"
         return tool
 
     #### Basic e2e tests
@@ -70,7 +70,7 @@ class TestE2EClientAsync:
         toolset = await toolbox.aload_toolset(toolset_name)
         assert len(toolset) == expected_length
         for tool in toolset:
-            name = tool._ToolboxTool__core_sync_tool.__name__
+            name = tool._ToolboxTool__core_tool.__name__
             assert name in expected_tools
 
     async def test_aload_toolset_all(self, toolbox):
@@ -84,7 +84,7 @@ class TestE2EClientAsync:
             "get-row-by-content-auth",
         ]
         for tool in toolset:
-            name = tool._ToolboxTool__core_sync_tool.__name__
+            name = tool._ToolboxTool__core_tool.__name__
             assert name in tool_names
 
     async def test_run_tool_async(self, get_n_rows_tool):
@@ -198,7 +198,7 @@ class TestE2EClientSync:
     @pytest.fixture(scope="function")
     def get_n_rows_tool(self, toolbox):
         tool = toolbox.load_tool("get-n-rows")
-        assert tool._ToolboxTool__core_sync_tool.__name__ == "get-n-rows"
+        assert tool._ToolboxTool__core_tool.__name__ == "get-n-rows"
         return tool
 
     #### Basic e2e tests
@@ -215,7 +215,7 @@ class TestE2EClientSync:
         toolset = toolbox.load_toolset(toolset_name)
         assert len(toolset) == expected_length
         for tool in toolset:
-            name = tool._ToolboxTool__core_sync_tool.__name__
+            name = tool._ToolboxTool__core_tool.__name__
             assert name in expected_tools
 
     def test_aload_toolset_all(self, toolbox):
@@ -229,7 +229,7 @@ class TestE2EClientSync:
             "get-row-by-content-auth",
         ]
         for tool in toolset:
-            name = tool._ToolboxTool__core_sync_tool.__name__
+            name = tool._ToolboxTool__core_tool.__name__
             assert name in tool_names
 
     @pytest.mark.asyncio
