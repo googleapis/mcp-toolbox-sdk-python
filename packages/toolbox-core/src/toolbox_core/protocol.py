0eae19554e61acb7e20a4acae015c2779ec4108d
Twisha Bansal <twishabansal@google.com>
2025-08-26 15:36:16 +0530

    lint

--- packages/toolbox-core/src/toolbox_core/protocol.py
+++ packages/toolbox-core/src/toolbox_core/protocol.py
@@ -40,9 +40,7 @@ class Protocol(str, Enum):
     @classmethod
     def get_supported_mcp_versions(cls):
         """Returns a list of supported MCP versions, sorted from newest to oldest."""
-        versions = [
-            member for member in cls if member.name.startswith("MCP_v")
-        ]
+        versions = [member for member in cls if member.name.startswith("MCP_v")]
         # Sort by the version date in descending order
         versions.sort(key=lambda x: x.value, reverse=True)
         return [v.value for v in versions]
