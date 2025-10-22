cb506473752d635f27a2a732b8f0e4ea3711e559
Ganga4060 <nainigangothri11@gmail.com>
2025-10-22 07:54:43 +0000

    testing export logs

---
+++ packages/toolbox-core/tests/test_failing.py
@@ -0,0 +1,3 @@
+def test_simple_failure():
+    """Intentionally fails to test CI failure detection."""
+    assert False, "This test is designed to fail"
--- packages/toolbox-core/tests/test_intentional_fail.py
+++
@@ -1,4 +0,0 @@
-def test_simple_failure():
-	# Intentionally fails to verify CI failure detection and log export
-	assert False, "Intentional failure"
-
