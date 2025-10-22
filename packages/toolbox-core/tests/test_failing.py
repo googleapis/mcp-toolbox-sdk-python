84bfab88ec784973555c58795d41638758a9d283
Ganga4060 <nainigangothri11@gmail.com>
2025-10-22 09:41:20 +0000

    test

--- packages/toolbox-core/tests/test_failing.py
+++ packages/toolbox-core/tests/test_failing.py
@@ -1,3 +1,3 @@
 def test_simple_failure():
     """Intentionally fails to test CI failure detection."""
-    assert False, "This test is designed to fail"
+    assert False, "This test is designed to fail intentionally"
