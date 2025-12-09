04d1d9cf9666f9e4de6a22b3aa8bd46d85f1ec43
Twisha Bansal <twishabansal07@gmail.com>
2025-12-10 02:14:26 +0530

    remove unwanted files

--- packages/toolbox-core/tests/test_sync_e2e.py
+++ packages/toolbox-core/tests/test_sync_e2e.py
@@ -141,7 +141,7 @@ class TestAuth:
         auth_tool = tool.add_auth_token_getters({"my-test-auth": lambda: auth_token2})
         with pytest.raises(
             Exception,
-            match="tool invocation not authorized. Please make sure your specify correct auth headers",
+            match="tool invocation not authorized",
         ):
             auth_tool(id="2")
 
