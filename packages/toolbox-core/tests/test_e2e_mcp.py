343e19708c919fada97495c08d8c2d238dfad045
Twisha Bansal <twishabansal07@gmail.com>
2025-12-22 14:49:47 +0530

    fix tests

--- packages/toolbox-core/tests/test_e2e_mcp.py
+++ packages/toolbox-core/tests/test_e2e_mcp.py
@@ -172,7 +172,7 @@ class TestAuth:
         auth_tool = tool.add_auth_token_getters({"my-test-auth": lambda: auth_token2})
         with pytest.raises(
             Exception,
-            match="tool invocation not authorized. Please make sure your specify correct auth headers",
+            match="401 (Unauthorized)",
         ):
             await auth_tool(id="2")
 
